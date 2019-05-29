1e4d664e72b96f0ddb619be15829c9230b1d0017
Bastian Koppelmann <kbastian@mail.uni-paderborn.de>
2019-05-29 13:21:33 +0200

    Always add tsmc65/sram libs to simulation
    Signed-off-by: Bastian Koppelmann <kbastian@mail.uni-paderborn.de>

--- vsim/tcl_files/config/vsim.tcl
+++ vsim/tcl_files/config/vsim.tcl
@@ -3,6 +3,8 @@ source tcl_files/config/vsim_ips.tcl
 set cmd "vsim -quiet $TB \
   -L pulpino_lib \
   $VSIM_IP_LIBS \
+  -L tsmc65_lib \
+  -L sram_tsmc65_lib \
   +nowarnTRAN \
   +nowarnTSCALE \
   +nowarnTFMPC \
