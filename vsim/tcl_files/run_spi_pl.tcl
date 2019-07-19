f25d774ec0667b7bf8f55eea3871701006c8cd12
Bastian Koppelmann <kbastian@mail.uni-paderborn.de>
2019-07-19 13:07:26 +0200

    tb: Add sdf to pl run script
    Signed-off-by: Bastian Koppelmann <kbastian@mail.uni-paderborn.de>

--- vsim/tcl_files/run_spi_pl.tcl
+++ vsim/tcl_files/run_spi_pl.tcl
@@ -6,5 +6,6 @@ set TB            tb_wrap_pl
 set TB_TEST $::env(TB_TEST)
 set VSIM_FLAGS    "-GTEST=\"$TB_TEST\""
 set MEMLOAD       "SPI"
+set SDF $::env(PL_SDF)
 
 source ./tcl_files/config/vsim_pl.tcl
